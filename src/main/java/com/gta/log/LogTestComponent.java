1c3c32c64236343d31a87946168de4efd92f9fb9
姜宁宁 <jiangningning@haohandata.com.cn>
2018-07-02 17:32:59 +0800

    init project

---
+++ src/main/java/com/gta/log/LogTestComponent.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2008-2015 浩瀚深度 All Rights Reserved.
+ *
+ * <p>FileName: LogTestController.java</p>
+ *
+ * @author jiangningning
+ * @date 2018/7/2
+ * @version 1.0
+ * History:
+ * v1.0.0, 姜宁宁 2018/7/2 Create
+ */
+package com.gta.log;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>Title: LogTest</p>
+ * <p>Description: </p>
+ * @author jiangningning
+ */
+@Component
+public class LogTestComponent {
+
+    public void test(){
+        System.out.println("AAAAAAAAAAAAAA");
+    }
+
+}
---
+++ src/test/java/com/gta/log/SsmCrudLogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gta.log;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsmCrudLogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
